009ea0b2b592f3c3079581dc4fe60fb2bbb1d344
vvduth <ducthai060501@gmail.com>
2025-09-01 17:22:46 +0300

    feat: Integrate course fetching with useGetCoursesQuery in landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useCarousel } from "@/hooks/use-carousel";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useGetCoursesQuery } from "@/state/api";
 
 const LoadingSkeleton = () => {
     return (
@@ -41,6 +42,8 @@ const LoadingSkeleton = () => {
 }
 const Landing = () => {
   const currentImage = useCarousel({ totalImages: 3 });
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+  console.log(courses);
   return (
     <motion.div
       initial={{
