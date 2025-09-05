3ddecf6b41385c3db5be22dc234322b721891999
vvduth <ducthai060501@gmail.com>
2025-09-05 16:13:43 +0300

    feat: Enhance transaction processing by adding transaction creation and user enrollment; update routes to include createTransaction

--- server/src/controllers/transactionController.ts
+++ server/src/controllers/transactionController.ts
@@ -1,42 +1,109 @@
-import { Request, Response } from 'express';
-import Stripe from 'stripe';
-import dotenv from 'dotenv';
+import { sectionSchema } from "./../../../asset-download/client/lib/schemas";
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import Course from "../models/courseModel";
+import Transaction from "../models/transactionModel";
+import UserCourseProgress from "../models/userCourseProgressModel";
 
 dotenv.config();
 
 if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+  throw new Error("STRIPE_SECRET_KEY is not defined in environment variables");
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-
 // payment intent is created when user wants to make a payment
-export const createStripePaymentIntent = async (req: Request, res: Response): Promise<void> => {
-    let { amount } = req.body;
+export const createStripePaymentIntent = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  let { amount } = req.body;
 
-    if (!amount || amount <= 0) {
-        amount = 50; // 50 cents minimum
-    }
+  if (!amount || amount <= 0) {
+    amount = 50; // 50 cents minimum
+  }
 
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount, 
-            currency: 'eur',
-            automatic_payment_methods: {
-                enabled: true,            
-                allow_redirects: "never"
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "eur",
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: "never",
+      },
+    });
+
+    // send client secret to the frontend so we can use it for information
+    res.json({
+      message: "Payment intent created successfully",
+      data: {
+        clientSecret: paymentIntent.client_secret,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create payment intent", error });
+  }
+};
+
+export const createTransaction = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, courseId, transactionId, amount, paymentProvider } = req.body;
+
+  try {
+    // Create a new transaction in the database
+
+    // 1.got course info
+    const course = await Course.get(courseId);
+
+    // 2. create transaction record
+    const newTransaction = new Transaction({
+      dateTime: new Date().toISOString(),
+      userId,
+      courseId,
+      transactionId,
+      amount,
+      paymentProvider,
+    });
+    await newTransaction.save();
+
+    // 3. create user course progress
+    const initialProgress = new UserCourseProgress({
+      userId,
+      courseId,
+      enrollmentDate: new Date().toISOString(),
+      overallProgress: 0,
+      sections: course.sections.map((section: any) => ({
+        sectionId: section.sectionId,
+        chapters: section.chapters.map((chapter: any) => ({
+          chapterId: chapter.chapterId,
+          cpmpleted: false,
+        })),
+      })),
+      lastAccessedTimestamp: new Date().toISOString(),
+    });
+    await initialProgress.save();
+
+    // 4. add user enrollement to course
+    await Course.update(
+        {courseId},
+        {
+            $ADD: {
+                enrollments: [{userId}]
             }
-        })
-
-        // send client secret to the frontend so we can use it for information
-        res.json({
-            message : 'Payment intent created successfully',
-            data: {
-                clientSecret: paymentIntent.client_secret
-            }
-        })
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to create payment intent',  error });
-    }
-}
+        }
+    )
+    res.json({
+        message: "Purchase successful and course enrolled",
+        data: {
+            transaction: newTransaction,
+            courseProgress: initialProgress,
+        }
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create transaction and enrollment", error });
+  }
+};
--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createStripePaymentIntent } from '../controllers/transactionController';
+import { createStripePaymentIntent, createTransaction } from '../controllers/transactionController';
 
 const router = express.Router();
 
+router.post('/', createTransaction)
 router.post('/stripe/payment-intent', createStripePaymentIntent);
+
 
 export default router;
